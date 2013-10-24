6b819c5fb9e83ac1b251fa1826d850d8f39f128a
Samir Otiv <samirotiv@fakeserver.com>
2013-10-24 14:39:07 +0530

    Initial Engine Code

---
+++ engine.c
@@ -0,0 +1,36 @@
+/*
+GAME ENGINE
+    Handles the graphical engine functions.
+______________________________________________
+
+FILE: engine.c
+
+*/
+
+#include <curses.h>
+
+
+/*
+###############################__________---MAJOR FUNCTIONS---__________###################################
+*/
+
+/*
+# FUNCTION engineInit
+#     - Initialize NCURSES
+#     - Set I/O Options
+#     - Ensure screen size
+*/
+void engineInit(){
+    (void) initscr();               //Initializing NCURSES
+
+/*TODO: SCREEN SIZE CHECKING*/
+    
+    raw();                          //Immediately record keystroke & don't interpret control characters
+    noecho();                       //Disable Echo
+    keypad(stdscr, TRUE);           //Enable input of control keys
+    curs_set(0);                    //Disable cursor
+    nodelay(stdscr, TRUE);          //Disable waiting for user input
+    
+    refresh();                      //First refresh of screen
+
+}
---
+++ engine.h
@@ -0,0 +1,19 @@
+/*
+GAME ENGINE
+    Handles the graphical engine functions.
+______________________________________________
+
+FILE: engine.h
+
+*/
+
+#ifndef ENGINE_DEFINED
+#define ENGINE_DEFINED
+
+
+
+//..################_________---FUNCTION DECLARATIONS---_________###############
+void engineInit();
+
+
+#endif
